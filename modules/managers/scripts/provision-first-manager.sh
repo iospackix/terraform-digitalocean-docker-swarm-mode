d752b14d3c6151b4bf40f9a03236aec287cbed6c
Andrew Wiik <11397337+andrewwiik@users.noreply.github.com>
2019-03-06 20:12:44 -0500

    Fix thing

--- modules/managers/scripts/provision-first-manager.sh
+++ modules/managers/scripts/provision-first-manager.sh
@@ -12,12 +12,17 @@ done
 sudo ufw status | grep -qw active
 UFW_INSTALLED=$?
 if [ $UFW_INSTALLED -eq 0 ]; then
+	echo "Apply UFW Rules"
+	echo "Apply UFW Rule :2337"
   sudo ufw allow 2377
+	echo "Apply UFW Rule :7946"
   sudo ufw allow 7946
+	echo "Apply UFW Rule :7946/udp"
   sudo ufw allow 7946/udp
+	echo "Apply UFW Rule :4798/udp"
   sudo ufw allow 4789/udp
 fi
 
-echo "Starting Swarm"
+echo "Starting Swarm with Address: $MANAGER_PRIVATE_ADDR"
 
 ${docker_cmd} swarm init --advertise-addr $MANAGER_PRIVATE_ADDR
